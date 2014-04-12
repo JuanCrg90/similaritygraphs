5e494dc2c34c42310b8c9bae03aae40ced4d39d4
Juan Carlos Ruiz <juancarlosruiz14@gmail.com>
2014-04-12 01:26:57 -0500

    Commit desde el Qt, agregada documentación de la clase corpus.h

--- corpus.cpp
+++ corpus.cpp
@@ -1,5 +1,78 @@
 #include "corpus.h"
 
-Corpus::Corpus()
+
+Corpus::Corpus() {}
+
+Corpus::Corpus(string fullText): fullText(fullText){}
+
+void Corpus::setText(string fullText)
 {
+    this->fullText=fullText;
+}
+
+string Corpus::getText()
+{
+    return this->fullText;
+}
+
+bool Corpus::generateFrecuencyTable()
+{
+
+
+
+    if(!fullText.empty())
+    {
+
+        std::map<string,int>::iterator it;
+        char *token;
+        char *dup = strdup(fullText.c_str());
+
+        token=strtok(dup,tags.c_str()) ;
+        while(token != NULL)
+        {
+            it=frecuencyTable.find(token);
+
+            if(it!=frecuencyTable.end())
+            {
+                it->second++;
+            }
+            else
+            {
+                frecuencyTable.insert(pair<string,int> (token,1));
+            }
+            token=strtok(NULL,tags.c_str());
+        }
+
+        free(dup);
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+void Corpus::showText()
+{
+    cout<<"----------------------------"<<endl;
+    cout<<"Full Text:"<<endl;
+    cout<<fullText<<endl;
+    cout<<"----------------------------"<<endl;
+}
+
+void Corpus::showFrecuencyTable()
+{
+    std::map<string,int>::iterator it;
+    it=frecuencyTable.begin();
+
+    cout<<"----------------------------"<<endl;
+    cout<<"Frequency Table:"<<endl;
+    while(it!=frecuencyTable.end())
+    {
+        cout<<it->first<<" "<<it->second<<endl;
+        it++;
+    }
+    cout<<"----------------------------"<<endl;
 }
